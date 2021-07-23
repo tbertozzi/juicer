13d8fb8ce3986a872c361e748661b13009c8a768
tbertozzi <terry.bertozzi@samuseum.sa.gov.au>
2021-07-23 11:10:05 +0930

    set defaults for NCI gadi & PBSpro syntax

--- PBS/scripts/postprocessing.sh
+++ PBS/scripts/postprocessing.sh
@@ -11,7 +11,7 @@ qsub <<- POSTPROCWRAP
 #PBS -S /bin/bash
 #PBS -q $queue
 #PBS -l $walltime
-#PBS -l nodes=1:ppn=1:AMD
+#PBS -l ncpus=1
 #PBS -l mem=4gb
 #PBS -o ${logdir}/${timestamp}_postproc_wrap_${groupname}.log
 #PBS -j oe
@@ -24,7 +24,7 @@ jID_hic30=\$(qstat | grep hic30_${groupname} |cut -d ' ' -f 1)
 
 if [ -z $postproc ]
 then
-    waitstring4="#PBS -W depend=afterok:\${jID_hic30}" 
+    waitstring4="#PBS -W depend=afterok:\${jID_hic30}"
 fi
 echo "waitstring4 is : \${waitstring4}"
 
@@ -33,8 +33,8 @@ qsub <<POSTPROCESS
     #PBS -S /bin/bash
     #PBS -q $queue
     #PBS -l $long_walltime
-    #PBS -l nodes=1:ppn=1:gpus=1:GPU 
-    #PBS -l mem=60gb
+    #PBS -l ncpus=16
+    #PBS -l mem=64gb
     #PBS -o ${logdir}/\${timestamp}_postproc_${groupname}.log
     #PBS -j oe
     #PBS -N PProc_${groupname}
@@ -43,7 +43,7 @@ qsub <<POSTPROCESS
     date +"%Y-%m-%d %H:%M:%S"
     $load_java
     $load_cuda
-    module list 
+    module list
     export _JAVA_OPTIONS=-Xmx16384m
     export LC_ALL=en_US.UTF-8
 
@@ -58,14 +58,14 @@ wait
 timestamp=$(date +"%s" | cut -c 4-10)
 qsub <<- FINCK
 #PBS -S /bin/bash
-#PBS -q $queue  
+#PBS -q $queue
 #PBS -l $walltime
 #PBS -o ${logdir}/${timestamp}_prep_done_${groupname}.log
 #PBS -j oe
 #PBS -N Pdone_${groupname}
 #PBS -W depend=afterok:${jID_postprocwrap}
 
-date +"%Y-%m-%d %H:%M:%S"    
+date +"%Y-%m-%d %H:%M:%S"
 jID_hic30=\$(qstat | grep hic30_${groupname} |cut -d ' ' -f 1)
 jID_stats0=\$(qstat | grep stats0${groupname} |cut -d ' ' -f 1)
 jID_stats30=\$(qstat | grep stats30${groupname} |cut -d ' ' -f 1)
@@ -82,17 +82,16 @@ qsub <<DONE
     #PBS -S /bin/bash
     #PBS -q $queue
     #PBS -l $walltime
-    #PBS -l nodes=1:ppn=1:AMD 
+    #PBS -l ncpus=1
     #PBS -l mem=4gb
     #PBS -o ${logdir}/\${timestamp}_done_${groupname}.log
     #PBS -j oe
     #PBS -N done_${groupname}
     \${waitstring5}
 
-    date +"%Y-%m-%d %H:%M:%S"    
+    date +"%Y-%m-%d %H:%M:%S"
     export splitdir=${splitdir}
     export outputdir=${outputdir}
     ${juiceDir}/scripts/check.sh
 DONE
 FINCK
-
